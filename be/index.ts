af739963a9140fe137417ddbf93fc92d7a7fb9e2
remigius.kalimba <remigius.kalimba@flexperto.com>
2023-11-23 16:11:58 +0100

    attempt to handle global async errors

--- be/index.ts
+++ be/index.ts
@@ -1,29 +1,36 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import { authMiddleware } from "./middleware/auth";
 import { acceptOnlyJson } from "./middleware/headers";
 import { connectMongoDB } from "./models/index";
 import router from "./routes/index";
 import cors from "cors";
+import globalErrorHandler, {
+  catchAsyncErrors,
+} from "./middleware/errorHandling";
+
+// Handle rejected promises globally
+catchAsyncErrors();
 
 connectMongoDB();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000'], // Replace with the origin of your React app
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: ["http://localhost:3000"], // Replace with the origin of your React app
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 app.use(acceptOnlyJson);
 
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // support json encoded bodies
 
 app.use(router);
+
+// Custom error-handling middleware
+app.use(globalErrorHandler);
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
--- be/middleware/errorHandling.ts
+++ be/middleware/errorHandling.ts
@@ -1,24 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 
-export const errorHandlingMiddleware = (
-  err: Error,
+export function catchAsyncErrors() {
+  process.on('unhandledRejection', (reason: any, promise: any) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Attach a custom error object to the promise
+    promise.customError = {
+      status: 500,
+      message: 'Internal Server Error',
+      reason,
+    };
+  });
+}
+
+export default function errorHandlingMiddleware(
+  error: any,
   req: Request,
   res: Response,
   next: NextFunction
-) => {
-  console.error(err.stack);
-
-  res.status(500).json({
-    error: "Internal Server Error",
-  });
-};
-
-export const handdleErrorAsync =
-  (func: Function) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await func(req, res, next);
-    } catch (error) {
-      next(error);
+){
+  // TODO: this is a good place to log the error to a file or database or some other logging service like Sentry
+  console.log('Error in error-handling middleware:', error);
+  try {
+    if (error.customError) {
+      // Handle the custom error here
+      const { status, message, reason } = error.customError;
+      res.status(status).json({ error: message, reason });
+    } else {
+      // Log and handle other errors
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+  } catch (err) {
+    console.error('Error in error-handling middleware:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
