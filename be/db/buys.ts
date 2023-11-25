eb0b96e6fabbe403b59378e9262cf952cada4b08
remigius.kalimba <remigius.kalimba@flexperto.com>
2023-11-25 18:37:05 +0100

    prefill products has wrong ids; tired people mistakes

--- be/db/buys.ts
+++ be/db/buys.ts
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 export default [
   {
     _id: new mongoose.Types.ObjectId("6561d8cbbd31e8ec42778013"),
-    productId: new mongoose.Types.ObjectId("6561d8bead0770d944c47a7e"),
+    productId: new mongoose.Types.ObjectId("656216496381cdf3c94d6018"),
     buyerId: new mongoose.Types.ObjectId("65616bf98efa03ab3f02d953"),
     sellerId: new mongoose.Types.ObjectId("65616ce394184915d9664327"),
   },
   {
     _id: new mongoose.Types.ObjectId("6561d8cfbd31e8ec42778024"),
-    productId: new mongoose.Types.ObjectId("6561d8bead0770d944c47a7f"),
+    productId: new mongoose.Types.ObjectId("656216496381cdf3c94d6016"),
     buyerId: new mongoose.Types.ObjectId("65616bf98efa03ab3f02d953"),
     sellerId: new mongoose.Types.ObjectId("65616ce394184915d9664327"),
   },
