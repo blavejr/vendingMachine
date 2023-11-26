522270b340eda4cd874cedb91798652fef2b032e
remigius.kalimba <remigius.kalimba@flexperto.com>
2023-11-26 02:13:18 +0100

    add more items in migrations so app makes sense to first time users and for pagination

--- be/db/products.ts
+++ be/db/products.ts
@@ -2,12 +2,30 @@ import mongoose from "mongoose";
 
 export default [
   {
+    _id: new mongoose.Types.ObjectId("656216496381cdf3c94d6342"),
+    amountAvailable: 35,
+    cost: 10,
+    productName: "juice",
+    image:
+      "https://www.dunnsfoodanddrinks.co.uk/content/images/big_product_images/3932.png",
+    sellerId: new mongoose.Types.ObjectId("65616ce394184915d9664327"),
+  },
+  {
     _id: new mongoose.Types.ObjectId("656216496381cdf3c94d6015"),
     amountAvailable: 35,
     cost: 10,
     productName: "chips",
     image:
       "https://www.freepnglogos.com/uploads/potato-chips-png/lays-classic-potato-chips-packet-png-image--0.png",
+    sellerId: new mongoose.Types.ObjectId("65616ce394184915d9664327"),
+  },
+  {
+    _id: new mongoose.Types.ObjectId("656216496381cdf3c94d6076"),
+    amountAvailable: 100,
+    cost: 10,
+    productName: "Noodles",
+    image:
+      "https://tradewindsorientalshop.co.uk/cdn/shop/products/Maggi_Chicken_Flavour_Noodles_81g_XX_1024x1024.jpg?v=1626783749",
     sellerId: new mongoose.Types.ObjectId("65616ce394184915d9664327"),
   },
   {
